bdf693c63a8f75a430967e54bda12d4d41e29dce
LookItsCashew <58984123+LookItsCashew@users.noreply.github.com>
2025-03-25 13:00:14 -0700

    Now creates credential xml file
    Adds declaration and root node to XML document.

--- CredentialManager/XML/CredentialXMLHandler.cs
+++ CredentialManager/XML/CredentialXMLHandler.cs
@@ -4,7 +4,9 @@ namespace CredentialManager.XML;
 
 public class CredentialXMLHandler
 {
-    private readonly string _workingDirectory = Directory.GetCurrentDirectory() + @"\vault\";
+    private readonly string _workingDirectory =
+        Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "vault" + Path.DirectorySeparatorChar;
+
     private FileInfo _credentialFile;
 
     public CredentialXMLHandler()
@@ -17,14 +19,30 @@ public class CredentialXMLHandler
         if (!_credentialFile.Exists)
         {
             _credentialFile.Create().Close();
+            BuildCredentialDocument();
         }
     }
 
-    public bool WorkingDirectoryExists() => Directory.Exists(_workingDirectory);
+    private bool WorkingDirectoryExists() => Directory.Exists(_workingDirectory);
     
-    public void CreateWorkingDirectory() => Directory.CreateDirectory(_workingDirectory);
+    private void CreateWorkingDirectory() => Directory.CreateDirectory(_workingDirectory);
+
+    private void BuildCredentialDocument()
+    {
+        var xml = new XmlDocument();
+        var decl = xml.CreateXmlDeclaration("1.0", "utf-8", null);
+        var root = xml.CreateElement("Credentials");
+        xml.AppendChild(decl);
+        xml.AppendChild(root);
+        xml.Save(_credentialFile.FullName);
+    }
 
     public void AddCredential(Credential credential)
+    {
+        
+    }
+
+    public void RemoveCredential(Credential credential)
     {
         
     }
