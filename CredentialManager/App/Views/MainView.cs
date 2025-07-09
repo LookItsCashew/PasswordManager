f09ded7f4d10c044f43c451890a4f6b12fb9bdf4
LookItsCashew <58984123+LookItsCashew@users.noreply.github.com>
2025-07-09 12:10:53 -0700

    MainView now uses panel instead of prompt
    This is to let me control how this page is interacted with instead of being stuck using the spectre prompt loop

--- CredentialManager/App/Views/MainView.cs
+++ CredentialManager/App/Views/MainView.cs
@@ -6,27 +6,62 @@ public class MainView : IView
 {
     private enum MainMenuChoices
     {
-        Credentials,
-        Groups
+        Credentials = 'c',
+        Groups = 'g'
     }
+    
     private IView SelectChoice(MainMenuChoices choice) => choice switch
     {
         MainMenuChoices.Credentials => new CredentialView(),
         MainMenuChoices.Groups => new ManageGroupsView(),
         _ => throw new ArgumentOutOfRangeException(nameof(choice), choice, null)
     };
+
+    private MainMenuChoices ChoiceListener()
+    {
+        while (true)
+        {
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case (char)MainMenuChoices.Credentials:
+                    return MainMenuChoices.Credentials;
+                case (char)MainMenuChoices.Groups:
+                    return MainMenuChoices.Groups;
+            }
+        }
+    }
     
     public void Render()
     {
         AnsiConsole.Write(new Rule("[yellow]Main Menu[/]"));
 
-        var selectedOption = AnsiConsole.Prompt(
-            new SelectionPrompt<MainMenuChoices>()
-                .AddChoices(
-                    MainMenuChoices.Credentials,
-                    MainMenuChoices.Groups));
+        string menuContents = "";
+        var choiceEnum = Enum.GetValues<MainMenuChoices>();
+        foreach (MainMenuChoices choice in choiceEnum)
+        {
+            menuContents = string.Concat(menuContents, 
+                "(" + Char.ToUpper((char)choice) + ")   ", 
+                choice);
+            menuContents += choiceEnum.Last() != choice ? "\n" : "";
+        }
         
-        var nextView = SelectChoice(selectedOption);
+        var mainMenuPanel = new Panel(menuContents)
+        {
+            Expand = true,
+            Border = BoxBorder.Double
+        };
+        
+        AnsiConsole.Write(mainMenuPanel);
+
+        var choiceSelected = ChoiceListener();
+
+        // var selectedOption = AnsiConsole.Prompt(
+        //     new SelectionPrompt<MainMenuChoices>()
+        //         .AddChoices(
+        //             MainMenuChoices.Credentials,
+        //             MainMenuChoices.Groups));
+        //
+        var nextView = SelectChoice(choiceSelected);
         AppEvents.TransitionSubViewEvent?.Invoke(nextView);
     }
 }
