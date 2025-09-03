2d28e7deebdeab5cb83db84baaab583870146472
LookItsCashew <58984123+LookItsCashew@users.noreply.github.com>
2025-09-02 17:00:25 -0700

    implemented simple email validation
    Not sure if I ever plan to fully integrate password recovery with email, but if so I'll at least have this

--- CredentialManager/Services/UserService.cs
+++ CredentialManager/Services/UserService.cs
@@ -3,6 +3,8 @@ using CredentialManager.Models;
 using CredentialManager.Database;
 using CredentialManager.Utils;
 using CredentialManager.Database.Repositories;
+using System.ComponentModel.DataAnnotations;
+using Spectre.Console;
 
 namespace CredentialManager.Services;
 
@@ -18,21 +20,37 @@ public class UserService
     
     public bool CheckLogin(User user) => LogIn(user);
 
+    public void Register(User user)
+    {
+        // validate email address
+        while (!ValidateEmail(user.Email))
+        {
+            // continue the loop to prompt user until an email of valid format is given
+            // right now, this ONLY checks if the email is of a valid format, not if the email is an existing address
+            AnsiConsole.MarkupLine($"[bold red]Warning: '{user.Email}' does not appear to be a valid address.[/]\n");
+            user.Email = AnsiConsole.Prompt(
+                new TextPrompt<string>("Please re-enter your email:")
+                );
+        }  
+
+        _repo.Create(user);
+    }
+
+    public bool ValidateEmail(string email)
+    {
+        EmailAddressAttribute emailValidator = new();
+        return email != "" && emailValidator.IsValid(email);
+    }
+
     bool LogIn(User user)
     {
         var results = _repo.ReadAll();
         if (results != null && results.Count > 0)
         {
-            return results.First().Username == user.Username && 
+            return results.First().Username == user.Username &&
                 results.First().Password == user.Password;
         }
 
         return false;
-    }
-
-    public void Register(User user)
-    {
-        // TODO: validate email address
-        _repo.Create(user);
     }
 }
---
+++ CredentialManagerTests/UserServiceUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CredentialManager.Services;
+
+namespace CredentialManagerTests;
+public class UserServiceUnitTest
+{
+    private readonly UserService us = new();
+
+    [Fact]
+    public void EmailWithFullyQualifiedDomainNameIsValid()
+    {
+        var email = "testemailaccount@gmail.com";
+        Assert.True(us.ValidateEmail(email));
+    }
+
+    [Fact]
+    public void EmailWithoutTLDIsNotValid()
+    {
+        var email = "estemailaccount@";
+        Assert.False(us.ValidateEmail(email));
+    }
+
+    [Fact]
+    public void EmailWithNonstandardTLDIsValid()
+    {
+        var email = "testaccount@thisismadeup.deez";
+        Assert.True(us.ValidateEmail(email));
+    }
+}
